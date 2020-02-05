19495f0302754e91f5cb0bf3d8c72f4165aed44c
Jini Byun <sungmin_byun@manulife.com>
2020-02-05 17:59:58 -0500

    commit

--- EFCoreConsole/Program.cs
+++ EFCoreConsole/Program.cs
@@ -1,6 +1,8 @@
 ﻿using EFCoreConsole.Model;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace EFCoreConsole
@@ -9,89 +11,234 @@ namespace EFCoreConsole
     {
         static void Main(string[] args)
         {
-            // 0. Query
-            // GetData();
-
             // 1. Add
-            // AddData();
+            //AddData("Steve")
+
+            // 1-1 
+            //var stdAddress = new StudentAddress()
+            //{
+            //    Address1 = "1 King",
+            //    City = "Toronto",
+            //};
+            //AddData("Jini", stdAddress);
 
             // 2. Update
-            UpdateData();
+            // UpdateData(1, "Jini");
 
             // 3. Delete
-            // DeleteData();
+            // DeleteData(1);
 
+            // 4. Query
+            // GetData();
+
+            // 5. Query
+            // GetData("Jini");         
+
+            // 6. Raw SQL (SELECT)
+            // GetDataUsingRawSQL();
+
+            // 7. Execute SQL (INSERT, UPDATE and DELETE) using Raw SQL
+            // ExecuteSqlCommand();
+
+            // 8. Transaction Support
+            // TransactionSupport();
+
+            // TODO
+            // bcp and ModelFirst
         }
 
         private static void GetData()
         {
             using (var context = new SchoolContext())
+            {                
+                // explanation
+                // What is difference between IQueryable and IEnumerable
+
+                var lstStudents = context.Students.ToList<Student>();
+                foreach (var member in lstStudents)
+                {
+                    Console.WriteLine(string.Format("Student Id: {0}, Student Name: {1}", member.StudentID, member.StudentName));
+                }
+            }
+        }
+
+        private static void GetData(string stdName)
+        {
+            using (var context = new SchoolContext())
             {
-                var stdName = "John";
-                var studentsWithSameName = context.Students
-                                                  .Where(s => s.StudentName == stdName)
-                                                  .ToList();
+                var studentsWithSameName = context.Students.Where(s => s.StudentName == stdName).Any() ? context.Students.Where(s => s.StudentName == stdName).ToList<Student>() : null;
 
-                Console.WriteLine("Studnent found: " + studentsWithSameName.Count);
+                var found = 0;
+                if (studentsWithSameName != null)
+                {
+                    found = studentsWithSameName.Count;
+                }
+                Console.WriteLine("Studnent found: " + found);
 
-                // Eager Loading
+                // explanation
+                // Difference Between Eager Loading and Lazy Loading
+                // ref: https://www.itorian.com/2013/06/what-is-eager-loading-and-what-is-lazy.html
+                // Eager Loading: It loads the related data in scalar and navigation properties along with query result at first shot.
+                // Lazy Loading:  EF loads only the data for the primary object in the LINQ query (the Friend) and leaves the Contact object.
+                // Lazy loading brings in the related data on an as-needed basis
+
+                // Eager Loading                
                 var gradeName = "no grade";
                 var studentWithGrade = context.Students
-                           .Where(s => s.StudentName == "Jini")
+                           .Where(s => s.StudentName == stdName)
                            .Include(s => s.Grade)
                            .FirstOrDefault();
                 if (studentWithGrade != null)
+                {
                     gradeName = studentWithGrade.Grade != null ? studentWithGrade.Grade.GradeName : "no grade";
-
-                Console.WriteLine(studentWithGrade.StudentName + " : " + gradeName);
-
+                    Console.WriteLine(studentWithGrade.StudentName + " : " + gradeName);
+                }
             }
         }
 
-        private static void DeleteData()
+        private static void GetDataUsingRawSQL()
         {
             using (var context = new SchoolContext())
             {
-                var std = context.Students.First<Student>();
-                context.Students.Remove(std);
+                var lstStudents = context.Students.FromSql("Select * from Students").ToList<Student>();
+                foreach (var member in lstStudents)
+                {
+                    Console.WriteLine(string.Format("Student Id: {0}, Student Name: {1}", member.StudentID, member.StudentName));
+                }
+                // var student = context.Students.FromSql("Select StudentId, StudentName, StandardId, RowVersion from Student where StudentId = 1").ToList();
 
-                // or
-                // context.Remove<Student>(std);
-
-                context.SaveChanges();
+                //TODO: Create stored proc and call using FromSql
             }
         }
 
-        private static void UpdateData()
+        private static void DeleteData(int stdId)
         {
+            if (stdId < 1) throw new Exception("student id should be greater than zero");
             using (var context = new SchoolContext())
             {
-                var std = context.Students.First<Student>();
+                // First and FirstOrDefault
+                var std = context.Students.FirstOrDefault<Student>();
 
-
-                
-
-
-                std.StudentName = "Steve";
-                context.SaveChanges();
+                if (std != null)
+                {
+                    context.Students.Remove(std);
+                    // or
+                    // context.Remove<Student>(std);
+                    context.SaveChanges();
+                }
+                else
+                {
+                    Console.WriteLine("No student has been found");
+                }
             }
         }
 
-        private static void AddData()
+        private static void UpdateData(int stdId, string stdName)
         {
+            if (stdId < 1) throw new Exception("student id should be greater than zero");
+            if (string.IsNullOrEmpty(stdName)) throw new Exception("student name cannot be null");
+
+            using (var context = new SchoolContext())
+            {
+                var std = context.Students.Where(x => x.StudentID == stdId).FirstOrDefault<Student>();
+
+                if (std != null)
+                {
+                    std.StudentName = stdName;
+                    context.SaveChanges();
+                }
+            }
+        }
+
+        private static void AddData(string stdName)
+        {
+            if (string.IsNullOrEmpty(stdName)) throw new Exception("student name cannot be null");
+
             using (var context = new SchoolContext())
             {
                 var std = new Student()
                 {
-                    StudentName = "Sunmi",
+                    StudentName = stdName,
                     DateOfBirth = DateTime.Parse("1999-12-12")
                 };
                 context.Students.Add(std);
-
                 // or
                 // context.Add<Student>(std);
 
                 context.SaveChanges();
+            }
+        }
+
+        private static void AddData(string stdName, StudentAddress stdAddress)
+        {
+            if (string.IsNullOrEmpty(stdName)) throw new Exception("student name cannot be null");
+
+            // TODO : validation of stdAddress
+
+            using (var context = new SchoolContext())
+            {
+                var std = new Student()
+                {
+                    StudentName = stdName,
+                    DateOfBirth = DateTime.Parse("1999-12-12"),
+                    StudentAddresses = new List<StudentAddress>() { stdAddress }
+                };
+
+
+                context.Students.Add(std);
+
+                context.SaveChanges();
+            }
+        }        
+
+        private static void ExecuteSqlCommand()
+        {
+            using (var context = new SchoolContext())
+            {
+                // TODO: using second parameter to pass database parameter to database
+
+                //Insert command
+                int noOfRowInsert = context.Database.ExecuteSqlCommand("insert into students(studentname) values('Robert')");
+
+                //Update command
+                int noOfRowUpdate = context.Database.ExecuteSqlCommand("Update students set studentname ='Mark' where studentname = 'Robert'");
+
+                //Delete command
+                int noOfRowDeleted = context.Database.ExecuteSqlCommand("delete from students where studentname = 'Mark'");
+            }
+        }
+
+        private static void TransactionSupport()
+        {
+            using (var context = new SchoolContext())
+            {               
+                using (IDbContextTransaction transaction = context.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        // process 1
+                        context.Students.Add(new Student()
+                        {
+                            StudentName = "CCC"
+                        });
+
+                        // process 2
+                        context.Courses.Add(new Course() { CourseName = "DDD" });
+                        
+                        
+                        context.SaveChanges();
+                        
+                        // pretend something happens here!!
+                        throw new Exception();
+
+                        transaction.Commit();
+                    }
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                        Console.WriteLine("Error occurred.");
+                    }
+                }
             }
         }
     }
--- EFCoreConsole/ReadMe.txt
+++ EFCoreConsole/ReadMe.txt
@@ -1,5 +1,7 @@
 ﻿## using dotnet cli, how to migrate, update database etc
 
+0. Make sure SQL server is accessbile
+
 1. preparation: to create the database from the model (entities and context) by adding a migration.
 dotnet ef migrations add CreateSchoolDB
 
@@ -8,3 +10,10 @@ dotnet ef database update
 
 3. NOTE: After upgrading to .net core 3.*, dotnet ef is missing  (from CLI)
 Therefore, we added global.json based on following url recommendation: https://github.com/dotnet/efcore/issues/18282
+
+4. confirm database table whether it is created
+
+5. then do exercise of Entity Framework in Program.cs
+
+6. refer to https://www.entityframeworktutorial.net/efcore/working-with-stored-procedure-in-ef-core.aspx to work with stored proc
+
